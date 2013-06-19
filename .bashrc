f162b6524a0269d19f58be321f33aa08670e97e4
Hemant Bhanoo <bhanoo@google.com>
2013-06-18 21:25:56 -0700

    bootstrap system, only common stuff

---
+++ .bashrc
@@ -0,0 +1,51 @@
+# .bashrc
+# This file should be in your home directory.
+# 
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+  . /etc/bashrc
+fi
+dotenv=~/dotenv
+if [ ! -d ${dotenv} ]; then
+  echo not sure where to find your environment. sorry.
+  exit
+fi
+
+_dotenv_find_files() {
+  directory=${1:-common} #private or common
+  if [ -d ${dotenv}/${directory} ]; then
+    find ${dotenv}/${directory} -type f -name ${2:-'*'} | sort
+  fi
+}
+
+_dotenv_source_files() {
+  if [[ "$DOTENV_DEBUG" -gt 0 ]]; then
+    echo "will source:"
+    _dotenv_find_files $@ | xargs -n1 echo
+    for f in $(_dotenv_find_files $@); do
+      read -p "[Enter] to source: $f"
+      source $f
+    done
+  else
+    for f in $(_dotenv_find_files $@); do
+      source $f
+    done
+  fi
+}
+
+_dotenv_debug() {
+  export DOTENV_DEBUG=${1:-1}
+  if [[ "$DOTENV_DEBUG" -gt 0 ]]; then
+    echo debug mode on
+  else
+    echo debug mode off
+  fi
+}
+
+bootstrap=${dotenv}/common/bash
+if [ -f ${bootstrap} ]; then
+  alias sp="source ${bootstrap}"
+  echo Type 'sp' to get your env started.
+else
+  echo bootstrap file not found!
+fi
---
+++ common/10_history.bash
@@ -0,0 +1,49 @@
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# TODO (bhanoo): move this to another file.
+
+SHARED_HOME=/home/${USER}
+SHARED_HISTORY_FILE=${SHARED_HOME}/.shared_history
+
+# Setting PROMPT_COMMAND=prompt_command will automatically notify after a run
+# finishes for a job that takes more than a minute.
+prompt_command() {
+  local history_output=$(HISTTIMEFORMAT="%s " history 1)
+  local history_number=${history_output/ */}
+  local rest=${history_output#*  }
+  local started_at=${rest/ */}
+  local last_command=${rest#* }
+  local finished_at=$(date +%s)
+  if [[ ! -z "$LAST_HISTORY_NUMBER" &&
+        "$history_number" != "$LAST_HISTORY_NUMBER" ]] &&
+    (( $finished_at - $started_at > 60 )); then
+    local elapsed_seconds=$(( $finished_at - $started_at ))
+    local ds=$((elapsed_seconds % 60))
+    local dm=$(((elapsed_seconds / 60) % 60))
+    local dh=$((elapsed_seconds / 3600))
+    local elapsed=$(printf '%d:%02d:%02d' $dh $dm $ds)
+    if [ -n "$DISPLAY" ]; then
+      notify-send "Finished in ${elapsed}:" "$last_command"
+    else
+      echo "Finished in ${elapsed}"
+    fi
+  fi
+  LAST_HISTORY_NUMBER=$history_number
+}
+
+# Log the commands only for user 'bhanoo'
+if [ $USER = bhanoo ]; then
+  PROMPT_COMMAND='echo `date` `tty` `pwd` `history 1` >> ${SHARED_HISTORY_FILE}'
+else
+  unset -v PROMPT_COMMAND
+fi
+
