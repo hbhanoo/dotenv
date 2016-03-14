b303f24d3fe79bc2e7b42bfbddbff7135879d15a
Hemant Bhanoo <github@hbhanoo.fastmail.fm>
2016-03-14 10:39:49 -0700

    add iterm cask, and instructions on how to customize it.

--- setup/mac/00_brew.sh
+++ setup/mac/00_brew.sh
@@ -7,4 +7,7 @@ brew update
 brew tap caskroom/cask
 brew tap homebrew/services
 echo installing core packages
-brew install bash bash-completion brew-cask coreutils findutils git libyaml openssl readline wget emacs
+brew install bash bash-completion brew-cask coreutils findutils git libyaml openssl readline wget emacs Caskroom/cask/iterm2
+
+echo setup iterm2 keybindings
+open http://brizzled.clapper.org/blog/2009/05/16/mac-os-x-iterm-bash-key-bindings-and-muscle-memory/
