71989c0fdf4d38ac4ebb88352de57e24ba70b191
Hemant Bhanoo <github@hbhanoo.fastmail.fm>
2016-09-23 10:14:09 -0500

    a more robust editor command that tries to use emacsclient when availalbe, falls back to emacs -nw. Also when invoked via ec, doesn't wait in the terminal. but when used as $EDITOR, does wait for the edit to finish.

--- common/00_editor.bash
+++ common/00_editor.bash
@@ -1,9 +1,9 @@
 if [ -x $HOME/dotenv/bin/editor ]; then
-   export EDITOR=$HOME/dotenv/bin/editor
+   export EDITOR="$HOME/dotenv/bin/editor"
 else
    export EDITOR=`which emacs`
 fi
 
 function ec() {
-		nohup $EDITOR $* 2>&1 > /tmp/nohup.out &
+		nohup $EDITOR -n "$@" 2>&1 > /tmp/nohup.out &
 }
