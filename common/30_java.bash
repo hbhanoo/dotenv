8b38b9d510f88cdf94ef85d5a94df449cf659a83
Hemant Bhanoo <github@hbhanoo.fastmail.fm>
2016-10-06 16:20:34 -0500

    make java recipe failure quiet

--- common/30_java.bash
+++ common/30_java.bash
@@ -1,3 +1,3 @@
 export JAVA6_HOME=`/usr/libexec/java_home -v 1.6.0_65-b14-466.1 2> /dev/null`
-export JAVA8_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA8_HOME=`/usr/libexec/java_home -v 1.8 2> /dev/null`
 export JAVA_HOME=$JAVA8_HOME
